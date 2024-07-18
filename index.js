78a9083fb7eec831381b94ea22ada9f5e1de4a89
firasmindhi123 <firas.inception@gmail.com>
2024-07-18 20:32:02 +0530

    contact detail

---
+++ index.js
@@ -0,0 +1,54 @@
+      
+   let index=-1
+    const data=[]
+  function  aq(){
+
+    
+
+    let name=document.getElementById('name').value
+    let email=document.getElementById('email').value
+    let phone=document.getElementById('phone').value
+    const obj={
+       name: name,
+       email: email,
+    phone:phone
+
+
+    }
+    if(index<0)
+        {
+            data.push(obj)  
+        }
+   else{
+        data[index]=obj
+   }
+   document.getElementById('name').value=''
+   document.getElementById('email').value=''
+   document.getElementById('phone').value=''
+   index=-1
+    display(data)
+  }
+   function  display(data)
+   {
+    let a=data.map((v,index)=>{
+        return `<tr><td>${v.name}</td>
+        <td>${v.phone}</td>
+        <td>${v.email}</td>
+        <td> <button onclick="remove(${index})">delete</button></td>
+        <td> <button  onclick="edit(${index})" >edit</button></td>
+        </tr>`
+    })
+    document.getElementById('listofitem').innerHTML=a.join('')
+   }
+   function edit(id){
+    index=id
+    document.getElementById('name').value=data[id].name
+    document.getElementById('phone').value=data[id].phone
+    document.getElementById('email').value=data[id].email
+
+   }
+  function remove(id){
+   data.splice(id,1)
+   display(data)    
+
+  }
