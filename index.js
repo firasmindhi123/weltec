b0107e77390eeecf91695db0dbbe1b5c94221bbf
firasmindhi123 <firas.inception@gmail.com>
2024-08-06 09:55:15 +0530

    time

--- index.js
+++ index.js
@@ -2,12 +2,18 @@
    let index=-1
     const data=[]
   function  aq(){
-
-    
-
     let name=document.getElementById('name').value
     let email=document.getElementById('email').value
-    let phone=document.getElementById('phone').value
+    let phone=document.getElementById('phone').value 
+    if(name=='' || email==''|| phone=='')
+    {
+        document.getElementById('name').value=''
+        document.getElementById('email').value=''
+        document.getElementById('phone').value=''
+          alert('field is empty')
+         return     
+    }
+    
     const obj={
        name: name,
        email: email,
@@ -52,3 +58,18 @@
    display(data)    
 
   }
+function search12()
+{
+  let filterData= document.getElementById('searchInput').value
+  console.log(filterData)
+  let sc = data.filter((data)=>{
+    return (data.name==filterData||data.phone==filterData||data.email==filterData)
+  })
+  if(sc)
+  {
+    display(sc)
+  }
+  else{
+    display(data)
+  }  
+}
